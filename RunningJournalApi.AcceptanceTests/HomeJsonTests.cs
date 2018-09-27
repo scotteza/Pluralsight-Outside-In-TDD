09eb64eb275bbc059286f7b74a042b065365b0d5
Scott Edwards <scotteza@gmail.com>
2018-09-27 09:48:05 +0100

    Base project with one failing acceptance test.

---
+++ RunningJournalApi.AcceptanceTests/HomeJsonTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Net.Http;
+
+namespace RunningJournalApi.AcceptanceTests
+{
+    [TestFixture]
+    public class HomeJsonTests
+    {
+        private Uri clientBaseAddress;
+
+        [SetUp]
+        public void SetUp()
+        {
+            clientBaseAddress = new Uri("http://localhost:9876");
+        }
+
+        [Test]
+        public void GetResponseReturnsCorrectStatusCode()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = clientBaseAddress;
+
+                var response = client.GetAsync("").Result;
+
+                Assert.IsTrue(
+                    response.IsSuccessStatusCode,
+                    "Actual status code: " + response.StatusCode);
+            }
+        }
+    }
+}
